22561a6dc30c75647a0098a757e244f5ba5fd038
youpy <youpy@buycheapviagraonlinenow.com>
2011-02-11 17:52:41 +0900

    add route: /soundcloud/download.mp3

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ app.get('/', function(req, res){
   res.send('H');
 });
 
+app.get('/soundcloud/download.mp3', function(req, res){
+  var downloadUrl = req.param('download_url');
+
+  if(downloadUrl) {
+    res.redirect(downloadUrl);
+  } else {
+    res.send('error', {}, 400);
+  }
+});
+
 app.listen(process.env.PORT || 8001);
