8e0b326f9c8b70e1c11b16155376d02b3bea5a59
youpy <youpy@buycheapviagraonlinenow.com>
2011-02-11 17:25:11 +0900

    use express

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('H\n');
+var app = express.createServer();
+
+app.get('/', function(req, res){
+  res.send('H');
 });
-server.listen(process.env.PORT || 8001);
+
+app.listen(process.env.PORT || 8001);
