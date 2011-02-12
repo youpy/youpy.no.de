45463c6bf9e86f75ecdc848c1dc8354f398ce9fd
youpy <youpy@buycheapviagraonlinenow.com>
2011-02-12 20:24:15 +0900

    add test()

--- test/server.test.js
+++ test/server.test.js
@@ -1,48 +1,52 @@
 var assert = require('assert');
 var app = require('../server');
 
-module.exports = {
-  '/soundcloud/download.mp3 with valid url': function() {
-    var url = 'http://api.soundcloud.com/tracks/8622419/download?consumer_key=xxxx';
+test('/soundcloud/download.mp3 with valid url', function() {
+  var url = 'http://api.soundcloud.com/tracks/8622419/download?consumer_key=xxxx';
 
-    assert.response(app, {
-      url: '/soundcloud/download.mp3?download_url=' +
-        encodeURIComponent(url)
-    }, {
-      statusCode: 302,
-      headers: {
-        location: url
-      }
-    });
-  },
-  '/soundcloud/download.mp3 with invalid url': function() {
-    var url = 'http://example.com/';
+  assert.response(app, {
+    url: '/soundcloud/download.mp3?download_url=' +
+      encodeURIComponent(url)
+  }, {
+    statusCode: 302,
+    headers: {
+      location: url
+    }
+  });
+});
 
-    assert.response(app, {
-      url: '/soundcloud/download.mp3?download_url=' +
-        encodeURIComponent(url)
-    }, {
-      statusCode: 400,
-      body: 'error'
-    });
-  },
-  '/soundcloud/download.mp3 with no url': function() {
-    assert.response(app, {
-      url: '/soundcloud/download.mp3'
-    }, {
-      statusCode: 400,
-      body: 'error'
-    });
-  }
-};
+test('/soundcloud/download.mp3 with invalid url', function() {
+  var url = 'http://example.com/';
+
+  assert.response(app, {
+    url: '/soundcloud/download.mp3?download_url=' +
+      encodeURIComponent(url)
+  }, {
+    statusCode: 400,
+    body: 'error'
+  });
+});
+
+test('/soundcloud/download.mp3 with no url', function() {
+  assert.response(app, {
+    url: '/soundcloud/download.mp3'
+  }, {
+    statusCode: 400,
+    body: 'error'
+  });
+});
 
 ['/', '/xxx', '/foo/bar'].forEach(function(path) {
-  module.exports[path] = function() {
+  test(path, function() {
     assert.response(app, {
       url: path
     }, {
       body: '',
       statusCode: 204
     });
-  };
+  });
 });
+
+function test(name, f) {
+  module.exports[name] = f;
+}
